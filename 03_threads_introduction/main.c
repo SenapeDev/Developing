89cff9eb045c5304fef9e8b91f2e85c95fcde783
Andrea <andrea@pop-os.localdomain>
2023-11-14 20:06:58 +0100

    added comments

--- 03_threads_introduction/main.c
+++ 03_threads_introduction/main.c
@@ -3,21 +3,25 @@
 
 void *print_char(void *p) {
 
+    // cast the void pointer to an int pointer
     int* ARGS;
     ARGS = (int *) p;
 
+    // print the character
     for(int i=0; i<ARGS[1]; i++) {
         printf("%c\n", ARGS[0]);
         fflush(stdout);
     }
 
     return NULL;
-
 }
 
 int main() {
 
+    // create a thread
     pthread_t t_ID;
+
+    // get user input
     int repetitions, character;
 
     printf("Enter a single character: ");
@@ -26,9 +30,13 @@ int main() {
     printf("How the number of repetition for letter '%c': ", character);
     scanf("%d", &repetitions);
 
+    // create a pointer to an array of arguments
     int ARGS[2] = {character, repetitions};
 
+    // create a thread
     pthread_create(&t_ID, NULL, &print_char, &ARGS);
+    
+    // wait for the thread to finish
     pthread_join(t_ID, NULL);
 
     return 0;
